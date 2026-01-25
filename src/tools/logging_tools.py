6a5cbc1e0e7753ab4563ce00edc109eb92da8c41
georgeaustin-jp <george.austin@challoners.org>
2026-01-25 20:12:41 +0000

    Changed logging level

--- src/tools/logging_tools.py
+++ src/tools/logging_tools.py
@@ -10,7 +10,7 @@ from tools.constants import Constants
 cr.init(autoreset=True)
 
 logging.basicConfig(
-  level=logging.DEBUG,
+  level=logging.CRITICAL,
   format=f"{cr.Fore.CYAN}%(levelname)s[%(name)s]{cr.Fore.WHITE} AT {cr.Fore.LIGHTMAGENTA_EX}\'%(filename)s\'{cr.Fore.WHITE} IN {cr.Fore.BLUE}\'%(funcName)s\'{cr.Fore.WHITE} {cr.Fore.LIGHTBLACK_EX}(line %(lineno)s):{cr.Style.RESET_ALL}\n >>> %(message)s",
 )
 
